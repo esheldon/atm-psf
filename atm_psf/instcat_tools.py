4ce7bc0ece170be1a02c965f590f700d0209dbea
Erin Sheldon <erin.sheldon@gmail.com>
2024-05-24 15:00:20 -0400

    rearrange commented stuff

--- atm_psf/instcat_tools.py
+++ atm_psf/instcat_tools.py
@@ -122,72 +122,6 @@ def replace_instcat_from_db(
     )
 
 
-# def replace_instcat(
-#     rng, fname, opsim_data, output_fname, allowed_include=None, sed=None,
-#     selector=None
-# ):
-#     """
-#     Replace the instacat metadata and positions according to the
-#     input opsim data
-#
-#     The ra/dec are generated uniformly in a disc centered at the
-#     boresight from the opsim data.
-#
-#     One can limit the objects in the output file with the input selector
-#     function and/or by limiting included source files with a list
-#     of strings for matching with allowed_include
-#
-#     Parameters
-#     ----------
-#     rng: np.random.default_rng
-#         The random number generator
-#     fname: str
-#         The input instcat filename
-#     opsim_data: mapping
-#         E.g. a sqlite.Row read from an opsim database.
-#     output_fname: str
-#         Name for new output instcat file
-#     allowed_include: list of strings
-#         Only includes with a filename that includes the string
-#         are kept.  For  ['star', 'gal'] we would keep filenames
-#         that had star or gal in them
-#     sed: str
-#         Force use if the input SED for all objects
-#         e.g starSED/phoSimMLT/lte034-4.5-1.0a+0.4.BT-Settl.spec.gz
-#     selector: function
-#         Evaluates True for objects to be kept, e.g.
-#             f = lambda d: d['magnorm'] > 17
-#     """
-#
-#     assert output_fname != fname
-#
-#     data, orig_meta = read_instcat(
-#         fname, allowed_include=allowed_include,
-#     )
-#
-#     if selector is not None:
-#         data = [d for d in data if selector(d)]
-#
-#    meta = replace_instcat_meta(
-#        rng=rng,
-#        meta=orig_meta,
-#        opsim_data=opsim_data,
-#    )
-#     replace_instcat_radec(
-#         rng=rng,
-#         ra=meta['rightascension'],
-#         dec=meta['declination'],
-#         data=data,
-#     )
-#
-#     if sed is not None:
-#         print('replacing SED with:', sed)
-#         for tdata in data:
-#             tdata['sed1'] = sed
-#
-#     write_instcat(output_fname, data, meta)
-
-
 def replace_instcat_meta(rng, meta, opsim_data):
     """
     Replace the metadata from an instcat with entries from an opsim
@@ -219,37 +153,6 @@ def replace_instcat_meta(rng, meta, opsim_data):
     new_meta['seed'] = rng.integers(0, 2**30)
     new_meta['seqnum'] = 0
     return new_meta
-
-
-def replace_instcat_radec(rng, ra, dec, data):
-    """
-    generate new positions for objects centered at the ra, dec
-
-    Parameters
-    ----------
-    rng: np.random.default_rng
-        The random number generator
-    ra: float
-        The ra of the new pointing
-    dec: float
-        The dec of the new pointing
-    data: list
-        List of instcat entries, as read with read_instcat
-    """
-    from esutil.coords import randcap
-    n = len(data)
-
-    rra, rdec = randcap(
-        nrand=n,
-        ra=ra,
-        dec=dec,
-        rad=INSTCAT_RADIUS,
-        rng=rng,
-    )
-
-    for i, d in enumerate(data):
-        d['ra'] = rra[i]
-        d['dec'] = rdec[i]
 
 
 class RadecGenerator():
@@ -929,6 +832,125 @@ def instcat_meta_to_wcs(meta, detnum):
     return factory.getWCS(det)
 
 
+def _get_opener(fname):
+    import gzip
+
+    if '.gz' in fname:
+        opener = gzip.open
+        mode = 'rt'
+    else:
+        opener = open
+        mode = 'r'
+
+    return opener, mode
+
+
+def path_split(fname):
+    import os
+
+    dirname, basename = os.path.split(fname)
+    if dirname == '':
+        dirname = './'
+    return dirname, fname
+
+
+# def replace_instcat(
+#     rng, fname, opsim_data, output_fname, allowed_include=None, sed=None,
+#     selector=None
+# ):
+#     """
+#     Replace the instacat metadata and positions according to the
+#     input opsim data
+#
+#     The ra/dec are generated uniformly in a disc centered at the
+#     boresight from the opsim data.
+#
+#     One can limit the objects in the output file with the input selector
+#     function and/or by limiting included source files with a list
+#     of strings for matching with allowed_include
+#
+#     Parameters
+#     ----------
+#     rng: np.random.default_rng
+#         The random number generator
+#     fname: str
+#         The input instcat filename
+#     opsim_data: mapping
+#         E.g. a sqlite.Row read from an opsim database.
+#     output_fname: str
+#         Name for new output instcat file
+#     allowed_include: list of strings
+#         Only includes with a filename that includes the string
+#         are kept.  For  ['star', 'gal'] we would keep filenames
+#         that had star or gal in them
+#     sed: str
+#         Force use if the input SED for all objects
+#         e.g starSED/phoSimMLT/lte034-4.5-1.0a+0.4.BT-Settl.spec.gz
+#     selector: function
+#         Evaluates True for objects to be kept, e.g.
+#             f = lambda d: d['magnorm'] > 17
+#     """
+#
+#     assert output_fname != fname
+#
+#     data, orig_meta = read_instcat(
+#         fname, allowed_include=allowed_include,
+#     )
+#
+#     if selector is not None:
+#         data = [d for d in data if selector(d)]
+#
+#    meta = replace_instcat_meta(
+#        rng=rng,
+#        meta=orig_meta,
+#        opsim_data=opsim_data,
+#    )
+#     replace_instcat_radec(
+#         rng=rng,
+#         ra=meta['rightascension'],
+#         dec=meta['declination'],
+#         data=data,
+#     )
+#
+#     if sed is not None:
+#         print('replacing SED with:', sed)
+#         for tdata in data:
+#             tdata['sed1'] = sed
+#
+#     write_instcat(output_fname, data, meta)
+
+
+# def replace_instcat_radec(rng, ra, dec, data):
+#     """
+#     generate new positions for objects centered at the ra, dec
+#
+#     Parameters
+#     ----------
+#     rng: np.random.default_rng
+#         The random number generator
+#     ra: float
+#         The ra of the new pointing
+#     dec: float
+#         The dec of the new pointing
+#     data: list
+#         List of instcat entries, as read with read_instcat
+#     """
+#     from esutil.coords import randcap
+#     n = len(data)
+#
+#     rra, rdec = randcap(
+#         nrand=n,
+#         ra=ra,
+#         dec=dec,
+#         rad=INSTCAT_RADIUS,
+#         rng=rng,
+#     )
+#
+#     for i, d in enumerate(data):
+#         d['ra'] = rra[i]
+#         d['dec'] = rdec[i]
+#
+
 # def write_instcat(fname, data, meta):
 #     """
 #     Write an instcat
@@ -958,25 +980,3 @@ def instcat_meta_to_wcs(meta, detnum):
 #             line = ' '.join(line)
 #             fobj.write(line)
 #             fobj.write('\n')
-
-
-def _get_opener(fname):
-    import gzip
-
-    if '.gz' in fname:
-        opener = gzip.open
-        mode = 'rt'
-    else:
-        opener = open
-        mode = 'r'
-
-    return opener, mode
-
-
-def path_split(fname):
-    import os
-
-    dirname, basename = os.path.split(fname)
-    if dirname == '':
-        dirname = './'
-    return dirname, fname
